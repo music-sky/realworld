d1bf5c42226f27c36db90eac3a0a0e678ec3f4b0
huangpeisong <895068878@qq.com>
2024-10-16 00:02:11 +0800

    http接口定义

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,6 +1,20 @@
 package service
 
-import "github.com/google/wire"
+import (
+	"github.com/google/wire"
+	v1 "kratos-realworld/api/realworld/v1"
+	"kratos-realworld/internal/biz"
+)
+
+type RealWorldService struct {
+	v1.UnimplementedRealWorldServer
+
+	uc *biz.GreeterUsecase
+}
 
 // ProviderSet is service providers.
 var ProviderSet = wire.NewSet(NewRealWorldService)
+
+func NewRealWorldService(uc *biz.GreeterUsecase) *RealWorldService {
+	return &RealWorldService{uc: uc}
+}
